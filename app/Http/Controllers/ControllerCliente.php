703ae13eec6b5b397bf507d061dfd98fe19e8901
wesllenalves <wesllenalves@gmail.com>
2018-10-30 11:18:19 -0200

    fazendo todos os comentarios dos metodos e decisões no controller de clinete

--- app/Http/Controllers/ControllerCliente.php
+++ app/Http/Controllers/ControllerCliente.php
@@ -11,39 +11,49 @@ class ControllerCliente extends Controller
 {
     private $user;
 
+    /* ================================== Metodo de Construtor ================================================== */
     public function __construct(User $user)
     {
         $this->user = $user;
     }
+
+    /* ======================= Metodo de renderização da VieW com todos os  clientes ============================= */
     public function index() 
-    {
+    {   
+        /** traz todos os dados existente na model*/
         $users = $this->user->all();
+        /** retorna a view com todos os dados encontrado */
         return view('samples.ClienteIndex', ['users' => $users]);
     }
 
+    /* ======================= Metodo de renderização da VieW de somente um cliente ============================= */
     public function cliente($id)
-    {
+    {   
+        /**traz somente os dados do id especifico*/
         $clientes = DB::table('users')->where('id', $id)->get();
-
+        /** retorna a view com os dados do usuarios achado */
         return view('samples.ClienteVisualizar', ['clientes' => $clientes]);
     }
 
+    /* ============================ Metodo de renderização da VieW de Editar cliente ========================== */
     public function clienteEditar($id)
-    {
-        
+    {   
+        /**traz somente os dados do id especifico*/
         $clientes = DB::table('users')->where('id', $id)->get();
-
+        /** retorna a view com os dados do usuarios achado */
         return view('samples.ClienteEditar', ['clientes' => $clientes]);
-
     }
 
+    /* ============================== Metodo de atualização de um cliente ======================================= */
     public function editar(Request $request, $id)
-    {
-        $dataForm = $request->all();      
-
+    {   
+        /**traz todos os dados vindo do formulario de atualização*/
+        $dataForm = $request->all();
+        /** retorna para a variavel o usuario especifico do id */
         $user = $this->user->find($id);
+        /** realiza o update com os dados do formulario */
         $update = $user->update($dataForm);        
-
+        /** faz a verificação para decidir para qual rota direcionar */
         if($update)
         {
             return redirect('/sample/cliente/visualizar/'.$id);
@@ -53,12 +63,16 @@ class ControllerCliente extends Controller
 
     }
 
+    /* ============================== Metodo de Renderização da View de clientes ======================================= */
     public function adicionar(){
         return view('samples.Adicionar-cliente');
     }
 
+    /* ============================== Metodo de Inserção de clientes ======================================= */
     public function adicionarCliente(Request $request){
+        /** recuperando os dados vindo do formulario e ignorando o campo de token */
        $dataFrom = $request->except('_token');
+       /** mensagens de possiveis erros */
        $messages = [
            'name.required' => 'O campo nome é de preenchimento obrigatorio.',
            'name.min' => 'O campo nome é de preenchimento de pelo menos 3 caracteres.',
@@ -75,10 +89,11 @@ class ControllerCliente extends Controller
            'endereco.required' => 'O campo endereco é de preenchimento obrigatorio.',
            'complemento.required' => 'O campo complemento é de preenchimento obrigatorio.',
        ];
+       /**usando help para fazer a validação, trazendo as regras da model USER */
        $this->validate($request, $this->user->rules, $messages);
-
+       /** inserindo os dados do formulario no banco */
        $insert = User::create($dataFrom);
-       
+       /** faz a verificação para decidir para qual rota direcionar */
        if($insert)
        {
              return redirect('/sample/cliente');
@@ -87,16 +102,18 @@ class ControllerCliente extends Controller
        }
     }
 
+    /* ============================== Metodo de Delete de clientes ======================================= */
     public function deletar($id){
-
+        /** recupera os dados do id especifico */
         $user = $this->user->find($id);
-       $delete =  $user->delete();
-       
-       if($delete)
-       {
-             return redirect('/sample/cliente');
-       }else{
-             return redirect('/sample/cliente');
-       }
+        /** realiza a remoção dos dados do cliente achado */
+        $delete =  $user->delete();
+        /** faz a verificação para decidir para qual rota direcionar */
+        if($delete)
+        {
+                return redirect('/sample/cliente');
+        }else{
+                return redirect('/sample/cliente');
+        }
     }
 }
