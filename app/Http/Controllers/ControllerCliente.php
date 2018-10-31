ccb20f28ed40316fc1367456318125a83a65b29c
wesllenalves <wesllenalves@gmail.com>
2018-10-31 11:05:31 -0200

    melhorando as regras do formulario tirando da model e colocando em uma clase que cuidarar somente disso e tirando as messagens perssonalizadas do controlles e colocando na ClienteFormRequest

--- app/Http/Controllers/ControllerCliente.php
+++ app/Http/Controllers/ControllerCliente.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\User;
+use App\Http\Requests\ClienteFormRequest;
 
 
 class ControllerCliente extends Controller
@@ -69,29 +70,15 @@ class ControllerCliente extends Controller
     }
 
     /* ============================== Metodo de Inserção de clientes ======================================= */
-    public function adicionarCliente(Request $request){
+    public function adicionarCliente(ClienteFormRequest $request){
         /** recuperando os dados vindo do formulario e ignorando o campo de token */
        $dataFrom = $request->except('_token');
-       /** mensagens de possiveis erros */
-       $messages = [
-           'name.required' => 'O campo nome é de preenchimento obrigatorio.',
-           'name.min' => 'O campo nome é de preenchimento de pelo menos 3 caracteres.',
-           'name.max' => 'O campo nome é de preenchimento de no maximo 100 caracteres.',
-           'cpf.required' => 'O campo cpf é de preenchimento obrigatorio.',
-           'cpf.numeric' => 'O campo cpf deve ser preenchido somente com numeros.',
-           'cpf.min' => 'O campo cpf deve ter no minimo 11 numeros.',
-           'cpf.max' => 'O campo cpf deve ter no maximo 11 numeros.',
-           'telefone.required' => 'O campo telefone é de preenchimento obrigatorio.',
-           'telefone.numeric' => 'O campo telefone é de preenchimento somente com  numeros.',
-           'telefone.min' => 'O campo telefone deve possuir no minimo 11 numeros.',
-           'telefone.max' => 'O campo telefone deve possuir no maximo 11 numeros.',
-           'email.required' => 'O campo email é de preenchimento obrigatorio.',
-           'endereco.required' => 'O campo endereco é de preenchimento obrigatorio.',
-           'complemento.required' => 'O campo complemento é de preenchimento obrigatorio.',
-       ];
+      
+       
        /**usando help para fazer a validação, trazendo as regras da model USER */
-       $this->validate($request, $this->user->rules, $messages);
+       //$this->validate($request, $this->user->rules, $messages);
        /** inserindo os dados do formulario no banco */
+
        $insert = User::create($dataFrom);
        /** faz a verificação para decidir para qual rota direcionar */
        if($insert)
--- app/User.php
+++ app/User.php
@@ -18,17 +18,7 @@ class User extends Authenticatable
 
     protected $fillable = [
         'name', 'cpf', 'telefone', 'email', 'endereco', 'cidade', 'complemento', 'password',
-    ];
-
-    /** regras de dados passado pelo formularios que serao aceitos */
-    public $rules = [
-        'name' => 'required|min:3|max:100',
-        'cpf' => 'required|numeric|min:11|max:11',
-        'telefone' => 'required|numeric|min:11|max:11',
-        'email' => 'required|email',
-        'endereco' => 'required|max:100',
-        'cidade' => 'required|max:100'
-    ];  
+    ];     
 
     /**
      * The attributes that should be hidden for arrays.
